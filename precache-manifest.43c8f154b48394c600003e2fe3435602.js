c5da726c4be32db5c2307fd3120870a93e12f2d6
anra1901 <am.ram06@hotmail.com>
2020-10-21 11:36:51 +0200

    Updates

---
+++ precache-manifest.43c8f154b48394c600003e2fe3435602.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dbf4a5e24d2f57fc5c6f9e0ac8be475",
+    "url": "/covid-app/index.html"
+  },
+  {
+    "revision": "397d2c769545f8892d15",
+    "url": "/covid-app/static/css/main.20886c22.chunk.css"
+  },
+  {
+    "revision": "99afbecee967875e77d0",
+    "url": "/covid-app/static/js/2.8a8dfc07.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-app/static/js/2.8a8dfc07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397d2c769545f8892d15",
+    "url": "/covid-app/static/js/main.baf10994.chunk.js"
+  },
+  {
+    "revision": "d85e7aeab982de52eafe",
+    "url": "/covid-app/static/js/runtime-main.073df564.js"
+  }
+]);
